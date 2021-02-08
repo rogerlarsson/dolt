62f291746f02acc18cc7db681bf2d1e841be7dc7
Brian Hendriks <brian@liquidata.co>
2021-02-08 10:32:19 -0800

    formatting

--- go/libraries/doltcore/sqle/index_row_iter.go
+++ go/libraries/doltcore/sqle/index_row_iter.go
@@ -15,8 +15,9 @@
 package sqle
 
 import (
-	"github.com/dolthub/go-mysql-server/sql"
 	"io"
+
+	"github.com/dolthub/go-mysql-server/sql"
 
 	"github.com/dolthub/dolt/go/libraries/doltcore/schema"
 	"github.com/dolthub/dolt/go/libraries/utils/set"
